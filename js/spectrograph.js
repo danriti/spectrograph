084112220f1b262b93959b02133bd783142c3cd2
Dan Riti <driti@tracelytics.com>
2013-10-12 19:54:28 +0000

    Move javascript into seperate file.

---
+++ js/spectrograph.js
@@ -0,0 +1,135 @@
+
+    // create the audio context (chrome only for now)
+    var context = new webkitAudioContext();
+    var audioBuffer;
+    var sourceNode;
+    var analyser;
+    var javascriptNode;
+
+    // get the context from the canvas to draw on
+    var ctx = $("#canvas").get()[0].getContext("2d");
+
+    // create a temp canvas we use for copying
+    var tempCanvas = document.createElement("canvas"),
+        tempCtx = tempCanvas.getContext("2d");
+    tempCanvas.width=800;
+    tempCanvas.height=512;
+
+    // used for color distribution
+    var color = new chroma.scale(['#000000', '#ff0000', '#ffff00', '#ffffff'])
+                          .mode('rgb')
+                          .domain([0, 300]);
+
+    // load the sound
+    setupAudioNodes();
+
+    function setupAudioNodes() {
+        // setup a javascript node
+        javascriptNode = context.createJavaScriptNode(2048, 1, 1);
+        // connect to destination, else it isn't called
+        javascriptNode.connect(context.destination);
+
+        // setup a analyzer
+        analyser = context.createAnalyser();
+        analyser.smoothingTimeConstant = 0;
+        analyser.fftSize = 1024;
+
+        // create a buffer source node
+        var mediaElement = document.getElementById('play');
+        console.log('mediaElement: ', mediaElement);
+        //sourceNode = context.createBufferSource();
+        sourceNode = context.createMediaElementSource(mediaElement);
+
+        console.log('sourceNode: ', sourceNode);
+
+        sourceNode.connect(analyser);
+        analyser.connect(javascriptNode);
+
+        sourceNode.connect(context.destination);
+    }
+
+    // load the specified sound
+    function loadSound(url) {
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.responseType = 'arraybuffer';
+
+        // When loaded decode the data
+        request.onload = function () {
+
+            // decode the data
+            context.decodeAudioData(request.response, function (buffer) {
+                // when the audio is decoded play the sound
+                playSound(buffer);
+            }, onError);
+        }
+        request.send();
+    }
+
+
+    function playSound(buffer) {
+        sourceNode.buffer = buffer;
+        sourceNode.start(0);
+        sourceNode.loop = false;
+    }
+
+    function stopSound(buffer) {
+        sourceNode.stop(0);
+    }
+
+    // log if an error occurs
+    function onError(e) {
+        console.log(e);
+    }
+
+    // when the javascript node is called
+    // we use information from the analyzer node
+    // to draw the volume
+    javascriptNode.onaudioprocess = function () {
+        // get the average for the first channel
+        var array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+
+        // draw the spectrogram
+        if (sourceNode.mediaElement && !sourceNode.mediaElement.paused) {
+            drawSpectrogram(array);
+        }
+    }
+
+    function drawSpectrogram(array) {
+        // copy the current canvas onto the temp canvas
+        var canvas = document.getElementById("canvas");
+
+        tempCtx.drawImage(canvas, 0, 0, 800, 512);
+
+        // iterate over the elements from the array
+        for (var i = 0; i < array.length; i++) {
+            // draw each pixel with the specific color
+            var value = array[i];
+            ctx.fillStyle = color(value).hex();
+
+            // draw the line at the right side of the canvas
+            ctx.fillRect(800 - 1, 512 - i, 1, 1);
+        }
+
+        // set translate on the canvas
+        ctx.translate(-1, 0);
+        // draw the copied image
+        ctx.drawImage(tempCanvas, 0, 0, 800, 512, 0, 0, 800, 512);
+
+        // reset the transformation matrix
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+    }
+
+    // Callbacks
+    $('#play').click(function() {
+        loadSound("/media/wagner-short.ogg");
+        //loadSound("/media/sweep.mp3");
+        //loadSound("/media/so_high.mp3");
+    });
+
+    // Callbacks
+    $('#stop').click(function() {
+        stopSound();
+    });
+
